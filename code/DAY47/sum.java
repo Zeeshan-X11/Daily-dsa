996cc011abb119bfaf99a05222bc6c58e9c48e22
zeeshan <189259933+Zeeshan-X11@users.noreply.github.com>
2026-07-17 22:42:45 +0530

    update

---
+++ code/DAY47/sum.java
@@ -0,0 +1,26 @@
+package DAY47;
+
+import java.util.*;
+
+class sum {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        map.put(0, 1); // important: handles sum == k case
+        
+        int sum = 0;
+        int count = 0;
+        
+        for (int num : nums) {
+            sum += num;
+            
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        
+        return count;
+    }
+}
